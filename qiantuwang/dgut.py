b663c538cfb604d05bfb2e9f59c64f5d1f27d23b
guguobao <1690259624@qq.com>
2018-04-01 22:49:22 +0800

    Update dgut.py

--- qiantuwang/dgut.py
+++ qiantuwang/dgut.py
@@ -1,5 +1,4 @@
-
-
+#!coding:utf-8
 import requests
 import re
 
@@ -7,21 +6,22 @@ import re
 
 
 if __name__=='__main__':
-    user=input("请输入学号：")
-    password=input("请输入密码：")
+    user=raw_input("请输入学号：")
+    password=raw_input("请输入密码：")
     data={
         'DDDDD':user,
         'upass':password,
-        '0MKKey':'(unable to decode value)'
+        '0MKKey':'123456',
     }
     headers={
         'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3141.7 Safari/537.36',
-        'Referer':'http://192.168.252.254/0.htm'
+        'Referer':'http://172.31.252.71/a70.htm?wlanuserip=172.27.169.186&wlanacip=null&wlanacname=null&vlanid=0&ip=172.27.169.186&ssid=null&areaID=null&mac=00-00-00-00-00-11m'
 
     }
-    req=requests.post('http://192.168.252.254/0.htm',data=data,headers=headers)
-    #print(req.text)`
-    re=re.findall(r'<title>(.*)</title>',req.text)
-    print(re)
+    req=requests.post('http://172.31.252.71:801/eportal/?c=ACSetting&a=Login&protocol=http:&hostname=172.31.252.71&iTermType=1&wlanuserip=172.27.169.186&wlanacip=null&wlanacname=null&mac=cc-2d-e0-6a-dc-72&ip=172.27.169.186&enAdvert=0&queryACIP=0&loginMethod=1',data=data,headers=headers)
+    #print(req.text)
+    ret=re.findall(r'<title>(.*)</title>',req.text)
+    ret=ret[0]
+    print(ret.encode('utf-8'))
 
 
